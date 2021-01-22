202626477525a68fbb6d6c2d26eb414fe733b9d5
aimehaum <aimehaum@gmail.com>
2021-01-23 00:53:28 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -35,6 +35,12 @@ def mark_todo(request, id):
     todo.save()
     return redirect(test)
 
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
 
 def add_book(request):
     form = request.POST
